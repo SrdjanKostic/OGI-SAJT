607d4b67315aa5cb1bf8a584a4e9ba522b2725d3
SrdjanKostic <124562894+SrdjanKostic@users.noreply.github.com>
2024-12-11 21:14:11 +0100

    Initial commit

---
+++ custom.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function($)
+{
+    $(".loader-wrapper").fadeOut();
+
+    $("#search-btn").click(function(e)
+    {
+        e.preventDefault();
+
+        $(".search-wrapper").fadeIn();
+    });
+    $("#close-search").click(function(e)
+    {
+        e.preventDefault();
+
+        $(".search-wrapper").fadeOut();
+    });
+    $(window).scroll(function()
+    {
+        if($(window).scrollTop()>200)
+        {
+            $("nav").addClass(' fixed-nav');
+        }
+        else
+        {
+            $("nav").removeClass(' fixed-nav');
+        }
+    });
+    $("#hamburger-menu-btn").click(function()
+    {
+        if($(this).hasClass('opened'))
+        {
+            $("nav ul").slideUp();
+        }
+        else
+        {
+            $("nav ul").slideDown();
+        }
+        $(this).toggleClass('opened');
+    });
+});
